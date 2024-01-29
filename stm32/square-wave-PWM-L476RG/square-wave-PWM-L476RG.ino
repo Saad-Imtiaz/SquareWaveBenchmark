328f32fe40b84632aac9758b766bbf1e279a268d
Saad Imtiaz <saadimtiazsial@gmail.com>
2024-01-29 13:40:43 +0500

    added STM32F407 D waveform

--- stm32/square-wave-PWM-L476RG/square-wave-PWM-L476RG.ino
+++ stm32/square-wave-PWM-L476RG/square-wave-PWM-L476RG.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-const int pwmPin = D5; // Change this to the PWM-capable pin you want to use
-const long frequency = 1000000; // Hz
+const int pwmPin = PA1; // Change this to the PWM-capable pin you want to use
+const long frequency = 10000000; // Hz
 const int resolution = 8; // 8-bit resolution
 const int dutyCycle = 128; // 50% duty cycle for 8-bit resolution (128 out of 256)
 
